5fc356765eea9703e19f79dc7e225fe4317fe1c7
tpwiedy <wiedy.pratama@gmail.com>
2024-02-06 16:54:44 +0700

    Challenge 10/366

---
+++ 10-stringToArray.js
@@ -0,0 +1,14 @@
+// WPU Coding Challenge 2024
+// 10/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+// Cara 1
+
+// function stringToArray(string) {
+//   return string.split(' ')
+// }
+
+// Cara 2
+const stringToArray = (string) => string.split(' ');
+
+console.log(stringToArray('Robin Singh'));
