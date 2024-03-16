c8e7431606963c27351a7288f6396ba9a05b955b
tpwiedy <wiedy.pratama@gmail.com>
2024-03-16 16:36:48 +0700

    Challenge 19/366

--- 19-sumMix.js
+++ 19-sumMix.js
@@ -1,6 +1,7 @@
 //WPU Coding Challenge 2024
 // 19/366
 //https://www.codewars.com/kata/57eaeb9578748ff92a000009
+// Cara 1
 // function sumMix(x) {
 //   let total = 0;
 //   for (let i = 0; i < x.length; i++) {
@@ -9,10 +10,12 @@
 //   return total;
 // }
 
+// Cara 2
 // function sumMix(x) {
 //   return x.reduce((acc, curr) => acc + +curr, 0);
 // }
 
+// Cara 3
 const sumMix = (x) => x.reduce((acc, curr) => acc + +curr, 0);
 
 console.log(sumMix([9, 3, '7', '3']));
